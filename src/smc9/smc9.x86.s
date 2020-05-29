05833805aff656fc8e3b4a6685a80102f2ed995c
SangYeoop <tkdduq8260@gmail.com>
2020-05-29 14:03:38 +0900

    Refactor : smc 9번 코드 코드 5번 복제하고 프로그램 중단.

--- src/smc9/smc9.x86.s
+++ src/smc9/smc9.x86.s
@@ -1,21 +1,28 @@
-section .data
 section .text
 
 global _start
 
+halt:
+    jmp halt; halt <- 0x8048060
+
 _start:
     mov eax, loop ; eax <- addr of loop
     mov ebx, new
-    mov ecx, new   
+    mov ecx, new
     mov esi, 2
+    mov edi, 10
+    mov ebp, 0
 loop:
-    mov edx, [eax] ; edx <- instruction of loop, but not worked
+    mov edx, [eax] ; edx <- instruction of loop
     mov [ebx], edx ; [ebx] <- store instruction of loop in pointer of ebx
     add eax, esi ; eax += 2, instruction length is 2byte
     add ebx, esi ; ebx += 2
-    cmp eax, ecx ; comparison about eax and ebx
+    cmp eax, ecx ; comparison about eax and ecx
     jne loop ; if not equal, than jump to the loop.
-    mov ecx, ebx ; if equal, all code is copied in new label, but not worked
+    mov ecx, ebx ; if equal, all code is copied in new label
+    add ebp, esi
+    cmp ebp, edi
+    je halt-100 ;halt is relative address, so if we repeat 5 times code, we need to subtract value 
 new:
-  
-  ;코드들이 복사되기는 하나, 계속 복사되다보니까 segmentation fault가 발생합니다.
+    
+
