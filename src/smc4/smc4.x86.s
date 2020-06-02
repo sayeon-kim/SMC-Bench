bffb14e1bf97a4255386a91d517c4ccb60ec45ae
spiderq2697 <65025333+spiderq2697@users.noreply.github.com>
2020-06-02 11:57:46 +0900

    Update smc4.x86.s

--- src/smc4/smc4.x86.s
+++ src/smc4/smc4.x86.s
@@ -22,3 +22,7 @@ addr:
 	mov edx, _start		; edx <- addr of _start
 	add edx, 10		; edx += 10 (mov eax, ebx 명령어주소)
 	jmp f			; jal f <- 0x804806e
+
+;13번째 코드를 보면 ecx == esp 로 같아야되는데 16bit 포인터를 이용한 상대점프를 사용하므로 
+;_start분기의 jmp f명령어와 addr분기의 jmp f명령어의 값이 다르다.
+;결국 ecx != esp 이므로 j halt 후 종료
