e5b8a4c40f378ff6c773300d483dda09861580cb
SangYeop <tkdduq8260@gmail.com>
2020-06-03 22:39:45 +0900

    smc8번 코드 변형 완료

--- src/smc8/smc8.x86.s
+++ src/smc8/smc8.x86.s
@@ -5,15 +5,18 @@ section .text
 global _start
 
 _start:
-    jmp alter
+    jmp alter           ; jmp alter
     mov [alter], ax
     
 alter:
-    mov ax, [_start]    ; eax <- load instruction of _start (jmp alter)
-    mov bl, 0x90        ; al <- load instruction of nop
-    mov [_start], bl    ; store address of nop in _start
+    mov ax, [_start]    ; ax <- load instruction of _start (jmp alter)
+    mov bl, 0x90        ; bl <- load instruction of nop
+    mov [_start], bl    ; store address of 'nop' in _start
     mov [_start+1], bl
     jmp _start          ; jmp _start
 
-    
-   
+;_start:
+;   nop
+;   mov [alter], ax
+;alter:
+;   jmp alter
