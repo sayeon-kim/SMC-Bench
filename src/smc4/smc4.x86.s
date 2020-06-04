b954cd52dac024a9247d0acfd19c651583cf9fdf
spiderq2697 <65025333+spiderq2697@users.noreply.github.com>
2020-06-04 15:54:35 +0900

    esp -> ebp 수정

--- src/smc4/smc4.x86.s
+++ src/smc4/smc4.x86.s
@@ -9,8 +9,8 @@ f:
 	mov ebx, 42		
 	mov ecx, [edx-2]	; ecx <- 0x8048068 (_start의 세번째 명령어 jmp f)
 	mov esi, addr		; esi <- addr of addr
-	mov esp, [esi+8]	; esp <- 0x8048093 (addr의 세번째 명령어 jmp f)
-	cmp ecx, esp		;if not equal, then jump to the halt//ecx == esp
+	mov ebp, [esi+8]	; esp <- 0x8048093 (addr의 세번째 명령어 jmp f)
+	cmp ecx, ebp		;if not equal, then jump to the halt//ecx == esp
 	jne halt
 	jmp [edx]			; [edx] = [0x0804806a] (mov eax, ebx/_start 4번째 명령어)
   
