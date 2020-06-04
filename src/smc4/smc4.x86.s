d571e776671cee2d269905d9c69aa4f4c681d55b
spiderq2697 <65025333+spiderq2697@users.noreply.github.com>
2020-06-04 16:47:05 +0900

    Update smc4.x86.s

--- src/smc4/smc4.x86.s
+++ src/smc4/smc4.x86.s
@@ -1,18 +1,18 @@
 _start:
 	mov edx, _start		; edx <- addr of _start
-	add edx, 10		; edx += 10 (mov eax, ebx 명령어주소)
+	add edx, 10		; edx += 10 (mov eax, ebx instruction address)
 	jmp f			; jal f <- 0x804806e
 	mov eax, ebx		
 	jmp halt		; j halt
   
 f:
 	mov ebx, 42		
-	mov ecx, [edx-2]	; ecx <- 0x8048068 (_start의 세번째 명령어 jmp f)
+	mov ecx, [edx-2]	; ecx <- 0x8048068 (3rd instruction of _start jmp f)
 	mov esi, addr		; esi <- addr of addr
-	mov ebp, [esi+8]	; esp <- 0x8048093 (addr의 세번째 명령어 jmp f)
+	mov ebp, [esi+8]	; esp <- 0x8048093 (3rd instruction of addr jmp f)
 	cmp ecx, ebp		;if not equal, then jump to the halt//ecx == esp
 	jne halt
-	jmp [edx]			; [edx] = [0x0804806a] (mov eax, ebx/_start 4번째 명령어)
+	jmp [edx]			; [edx] = [0x0804806a] (mov eax, ebx/_start 4th instruction)
   
 halt:
 	mov eax, 1
@@ -20,7 +20,7 @@ halt:
   
 addr:
 	mov edx, _start		; edx <- addr of _start
-	add edx, 10		; edx += 10 (mov eax, ebx 명령어주소)
+	add edx, 10		; edx += 10 (mov eax, ebx instruction address)
 	jmp f			; jal f <- 0x804806e
 
 ;13번째 코드를 보면 ecx == esp 로 같아야되는데 16bit 포인터를 이용한 상대점프를 사용하므로 
