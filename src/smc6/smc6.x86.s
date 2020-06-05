e3b90cfe7f8469a062564f9b96491a1d3fd3d212
sayeon-kim <52534862+sayeon-kim@users.noreply.github.com>
2020-06-05 11:52:47 +0900

    smc6번 코드 변형 완료

--- src/smc6/smc6.x86.s
+++ src/smc6/smc6.x86.s
@@ -6,16 +6,16 @@ global _start
 
 _start:
     mov eax, gen        ; eax <- load address of gen
-    mov ebx, 0x8048095  ; ebx <- load address of 0x8048095(halt)
+    mov ebx, 0x8048097  ; ebx <- load address of 0x8048095(halt)
     mov edx, [ebx]
     mov [eax], edx      ; eax <- store instruction of ebx(mov ecx, ebx) 
-    mov ebx, 0x8048097 ; ebx <- load address of 0x8048097(halt+2)
+    mov ebx, 0x8048099 ; ebx <- load address of 0x8048097(halt+2)
     add eax, 4          ; eax <- load address of (gen+4)
     mov edx, [ebx] 
     mov [eax], edx      ; eax <- store instruction of ebx(jmp ecx)
     mov ecx, ggen       ; ecx <- load address of ggen
     mov eax, _start     ; eax <- load address of _start
-    mov ebx, 0x8048099  ; ebx <- load address of 0x8048099(halt+4)
+    mov ebx, 0x804809b  ; ebx <- load address of 0x8048099(halt+4)
     mov edx, [ebx]
     mov ebx, edx
     jmp gen             ; jump gen
@@ -26,6 +26,8 @@ gen:
     nop
     nop
 ggen:
+    nop
+    nop
     nop
     nop
 
