f48f3e4f213bc11dd86b2f72e0a779c99c7068d9
SangYeoop <tkdduq8260@gmail.com>
2020-07-09 12:51:57 +0900

    SMC written by C

---
+++ src/c_smc/c_smc1/src/smc1.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/mman.h>
+
+void foo(void);
+int change_page_permissions_of_address(void *addr);
+
+int main(void)
+{
+	void *foo_addr = (void *)foo;
+
+	if (change_page_permissions_of_address(foo_addr) == -1)
+	{
+		fprintf(stderr, "Error while changing page permissions of foo(): %s\n", strerror(errno));
+		return 1;
+	}
+
+	puts("Calling foo...");
+	foo();
+
+	unsigned char *instruction = (unsigned char *)foo_addr + 18;
+	*instruction = 0x2A;
+
+	puts("Calling foo...");
+	foo();
+
+	return 0;
+}
+
+void foo(void)
+{
+	int i = 0;
+	i++;
+	printf("i: %d\n", i);
+}
+
+int change_page_permissions_of_address(void *addr)
+{
+	int page_size = 4096;
+	addr -= (unsigned long)addr % page_size;
+
+	if (mprotect(addr, page_size, PROT_READ | PROT_WRITE | PROT_EXEC) == -1)
+	{
+		return -1;
+	}
+
+	return 0;
+}
