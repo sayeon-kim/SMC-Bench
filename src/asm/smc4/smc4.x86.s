6a9bf66e18e5c3cb53103d83b26070a00344c00e
spiderq2697 <spiderq10@gmail.com>
2020-07-16 16:32:38 +0900

    update smc4.x86.s

--- src/asm/smc4/smc4.x86.s
+++ src/asm/smc4/smc4.x86.s
@@ -1,8 +1,8 @@
 _start:
 	mov edi, _start		; edi <- addr of _start
-	add edi, 10		; edi += 10 (mov eax, ebx instruction address)
+	add edi, 10		; edi += 10 (mov ebp, ebx instruction address)
 	jmp f			; jal f <- 0x88048071
-	mov eax, ebx		
+	mov ebp, ebx		
 	jmp halt		; j halt
   
 f:
@@ -12,7 +12,7 @@ f:
 	mov dl, [esi+8]	; esp <- 0x8048093 (3rd instruction of addr jmp f)
 	cmp cl, dl		;if not equal, then jump to the halt
 	jne halt
-	jmp edi			; [edx] = [0x0804806a] (mov eax, ebx/_start 4th instruction)
+	jmp edi			; [ebp] = [0x0804806a] (mov ebp, ebx/_start 4th instruction)
   
 halt:
 	mov eax, 1
@@ -20,9 +20,13 @@ halt:
   
 addr:
 	mov edi, _start		; edi <- addr of _start
-	add edi, 10		; edi += 10 (mov eax, ebx instruction address)
+	add edi, 10		; edi += 10 (mov ebp, ebx instruction address)
 	jmp f			; jal f <- 0x88048071
 
 ;13번째 코드를 보면 ecx == esp 로 같아야되는데 16bit 포인터를 이용한 상대점프를 사용하므로 
 ;_start분기의 jmp f명령어와 addr분기의 jmp f명령어의 맨 앞 1바이트만 가지고 와서 비교한다.
 ;ecx == esp 이므로 jmp edi로 넘어간다.
+
+;nasm -felf32 smc4.x86.s && ld -m elf_i386 --omagic smc4.x86.o -o smc4.x86 && ./smc4.x86
+;objdump smc4.x86 -d
+;gdb smc4.x86
