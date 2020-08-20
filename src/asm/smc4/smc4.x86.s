94af370e6b97fbc53557a8799834ce595b12e8de
spiderq2697 <spiderq10@gmail.com>
2020-08-20 22:51:29 +0900

    update smc4.x86.s

--- src/asm/smc4/smc4.x86.s
+++ src/asm/smc4/smc4.x86.s
@@ -1,17 +1,19 @@
 section .text
 global _start
 _start:
-	call f		; jal f
+	nop
+	mov eax, 0x804806c
+	call eax		; jal f
 	mov ebx, edi	; move $2, $8	
 	jmp halt		; j halt
   
 f:
 	mov edi, 42	
 	mov ebp, [esp]	
-	mov cl, [ebp-5]	
+	mov cx, [ebp-2]	
 	mov esi, addr		; esi <- addr of addr
-	mov dl, [esi]
-	cmp cl, dl	
+	mov dx, [esi+5]
+	cmp cx, dx	
 	jne halt
 	ret			
   
@@ -20,11 +22,13 @@ halt:
 	int 0x80		; j halt
   
 addr:
-	call f
+	mov eax, 0x804806c
+	call eax
 
-;13번째 코드를 보면 ecx == esi 로 같아야되는데 16bit 포인터를 이용한 상대점프를 사용하므로 
-;_start분기의 jmp f명령어와 addr분기의 jmp f명령어의 맨 앞 1바이트만 가지고 와서 비교한다.
-;ecx == esi 이므로 ret에 해당하는 esp주소로 분기된다.
+;gdb 명령어가 들어가는 nop을 _start 분기 첫번째에 생성하였다. break를 _start분기에 걸기를 추천한다.
+;절대주소를 이용하여 call f를 실행하였다.
+;call f 명령어는 2바이트 크기를 가지므로 _start의 call eax 2바이트, addr의 call eax 2바이트를 각각 cx, dx에 넣어 비교한다.
+;cx == dx 이므로 ret에 해당하는 esp주소로 분기된다.
 
 ;nasm -felf32 smc4.x86.s && ld -m elf_i386 --omagic smc4.x86.o -o smc4.x86 && ./smc4.x86
 ;objdump smc4.x86 -d
