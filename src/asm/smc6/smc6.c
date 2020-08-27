b343cab8f8c85f8f95cb5c8f16866bb1b2bf37b0
sayeon-kim <52534862+sayeon-kim@users.noreply.github.com>
2020-08-27 15:45:23 +0900

    Update smc6.c

--- src/asm/smc6/smc6.c
+++ src/asm/smc6/smc6.c
@@ -11,7 +11,16 @@ void foo();
 void modify();
 
 char *start_string = "start\n";
-char smc_string[] = "Self Modifing Code\n";
+char smc_string[] =  "\x48\x31\xd2"							   // xor    %rdx, %rdx
+		"\x48\x31\xc0"							   // xor    %rax, %rax
+		"\x48\xbb\x2f\x62\x69\x6e\x2f\x73\x68\x00" // mov    $0x68732f6e69622f2f, %rbx
+		"\x53"									   // push   %rbx
+		"\x48\x89\xe7"							   // mov    %rsp, %rdi
+		"\x50"									   // push   %rax
+		"\x57"									   // push   %rdi
+		"\x48\x89\xe6"							   // mov    %rsp, %rsi
+		"\xb0\x3b"								   // mov    $0x3b, %al
+		"\x0f\x05";		
 char add[] = "restart";
 
 int main(void)
