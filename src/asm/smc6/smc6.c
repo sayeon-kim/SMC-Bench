ff03d7f56d8d13f26e34030f3b6b032ed28fd3f8
sayeon-kim <52534862+sayeon-kim@users.noreply.github.com>
2020-09-03 18:45:23 +0900

    Update smc6.c

--- src/asm/smc6/smc6.c
+++ src/asm/smc6/smc6.c
@@ -8,40 +8,33 @@
 int change_page_permissions_of_address(void *addr);
 void get_permission(void *foo_addr);
 void foo();
-void modify();
 
+int num =1;
 char *start_string = "start\n";
-char smc_string[] =  "\x48\x31\xd2"							   // xor    %rdx, %rdx
-		"\x48\x31\xc0"							   // xor    %rax, %rax
-		"\x48\xbb\x2f\x62\x69\x6e\x2f\x73\x68\x00" // mov    $0x68732f6e69622f2f, %rbx
-		"\x53"									   // push   %rbx
-		"\x48\x89\xe7"							   // mov    %rsp, %rdi
-		"\x50"									   // push   %rax
-		"\x57"									   // push   %rdi
-		"\x48\x89\xe6"							   // mov    %rsp, %rsi
-		"\xb0\x3b"								   // mov    $0x3b, %al
-		"\x0f\x05";		
-char add[] = "restart";
-
+								   
 int main(void)
 {
+	void *instruction = (void *)main + 169;
+	void *instruction2 = (void *)main + 138;
 	get_permission(main);
 	goto START;
 
 MODIFY:
-	modify();
-	memcpy(modify, smc_string, sizeof(smc_string) - 1);
+	memcpy(instruction, instruction2, sizeof(instruction2) - 1);
+	goto GEN;
 
 START:
 	printf("It's main \n");
 	goto MODIFY;
 
+GEN:
+	num+=2;
+	printf("num is %d\n", num);
+	num-=1;
+	goto START;
+
 }
 
-void modify()
-{
-	printf("It's modify()\n");
-}
 
 void get_permission(void *foo_addr)
 {
