d453d18bbcd5a5716b66ab5811a06198d03e6de4
Cogi <dbwodlf3@naver.com>
2020-09-14 16:55:55 +0900

    update constrainter

---
+++ src/tools/constrainter/test.ll
@@ -0,0 +1,9 @@
+; Test ll fILE
+
+target datalayout = "e-m:e-i64:64-f80:128-n8:16:32:64-S128"
+target triple = "x86_64-pc-linux-gnu"
+
+define dso_local i32 @main() {
+  
+  ret i32 0
+}
---
+++ src/tools/constrainter/test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import os, argparse
+import subprocess
+
+parser = argparse.ArgumentParser()
+parser.add_argument("testWay",help="0: compile & test Pass\n1: compile\n2:test Pass")
+
+args = parser.parse_args()
+
+# Configure Variable
+dir_path = os.path.dirname(os.path.abspath(__file__))
+
+# Compile
+def compile():
+	p1 = subprocess.Popen(["cmake", ".", "-B./dest"], stdout=subprocess.PIPE)
+	text_bytes, err = p1.communicate()
+	print(text_bytes.decode('utf-8'))
+
+	p2 = subprocess.Popen(["cmake", "--build", "./dest"], stdout=subprocess.PIPE)
+	text_bytes, err = p2.communicate()
+	print(text_bytes.decode('utf-8'))
+
+if args.testWay == "0":
+	compile()
+if args.testWay == "1":
+	compile()
