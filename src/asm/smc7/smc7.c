2338ae4e7c5112bf60f060d77e7029c19461ce0b
Kwanghoon Choi <lazyswamp@gmail.com>
2020-09-14 16:14:53 +0900

    updaetd smc7.c

--- src/asm/smc7/smc7.c
+++ src/asm/smc7/smc7.c
@@ -6,26 +6,66 @@
 #include <sys/mman.h>
 
 int change_page_permissions_of_address(void *addr);
-void* _memcpy(void *dest, const void *src, size_t n);
 void get_permission(void *foo_addr);
-void foo();
 
 char *err_string = "Error while changing page permissions of foo()\n";
 char *smc_string = "Self Growing Code\n";
 
+#define SIZE_LOOP  68   //
+
 int main(void)
 {
-    get_permission(foo);
+  unsigned char* ptr_loop;
+  unsigned char* ptr_new;
 
-    int (*fp_printf)(const char*, ...);
-    void* (*fp_memcpy)(void *, const void* , size_t );
-    
-    fp_memcpy = memcpy;
-    fp_printf = printf;
+  unsigned char* ptr_$8;
+  unsigned char* ptr_$9;
+  unsigned char* ptr_$10;
 
-    foo(fp_memcpy, fp_printf);
+  int i;
+
+  get_permission(main);
+
+  // Initialize
+  ptr_loop = (unsigned char*) main + 66;  //
+  ptr_new  = (unsigned char*) main + 134;  //
+
+
+start:
+  // la $8, loop
+  ptr_$8 = ptr_loop;
+
+  // la $9, new
+  ptr_$9 = ptr_new;
+
+  // move $10, $9
+  ptr_$10 = ptr_$9;
+
+loop:
+
+  // loop:
+  // lw $11, 0($8)
+  // sw $11, 0($9)
+  // addi $8, $8, 4
+  // addi $9, $9, 4
+  // bne $8, $10, loop
+  for(i=0; i<SIZE_LOOP; i++)
+    ptr_new[i] = ptr_loop[i];
+
+  ptr_$8 = ptr_$8 + SIZE_LOOP;
+  ptr_$9 = ptr_$9 + SIZE_LOOP;
+
+  ptr_$10 = ptr_$9;
+
+new:
+
+  return 0;
+
 }
 
+
+
+// Permission related function
 void get_permission(void *foo_addr)
 {
     if (change_page_permissions_of_address(foo_addr) == -1)
@@ -49,9 +89,3 @@ int change_page_permissions_of_address(void *addr)
     return 0;
 }
 
-void foo(void* (*fp_memcpy)(void *, const void*, size_t), int (*fp_printf)(const char*, ...)) {
-    int num = 89;
-    fp_memcpy(foo + num, foo + 23, 66);
-    fp_printf("%d\n", num);
-    num += 66;
-}
