9a50db46fbbec3fbd35b6ccac177366c6a84e8ff
wespito <37640219+wespito@users.noreply.github.com>
2020-10-06 20:18:43 +0900

    changed smc9.x86.s (#25)

--- src/asm/smc9/smc9.x86.s
+++ src/asm/smc9/smc9.x86.s
@@ -1,25 +1,44 @@
 section .text
 global _start
 
-halt:
-    jmp halt; halt <- 0x8048060
-
 _start:
-    mov eax, loop ; eax <- addr of loop
-    mov ebx, new
-    mov ecx, new
-    mov esi, 4
-    mov edi, 20
-    mov ebp, 0
-loop:
-    mov edx, [eax] ; edx <- instruction of loop
-    mov [ebx], edx ; [ebx] <- store instruction of loop in pointer of ebx
-    add eax, esi ; eax += 4, instruction length is 2byte 2�پ� ����
-    add ebx, esi ; ebx += 4
-    cmp eax, ecx ; comparison about eax and ecx
-    jne loop ; if not equal, than jump to the loop.
-    mov ecx, ebx ; if equal, all code is copied in new label
-    add ebp, esi
-    cmp ebp, edi
-    je halt-100 ;halt is relative address, so if we repeat 5 times code, we need to subtract value 
-new:
+    mov eax, pg             ; eax <- load address of pg
+    mov ebx, pgend          ; ebx <- load address of pgend
+    mov ecx, 0xffffffff     ; ecx <- assign value of 0xffffffff
+
+xor1:
+    mov edx, [eax]          ; store instruction that pointed by eax in edx
+    xor edx, ecx            ; xor edx, ecx, and store result value in edx
+    mov [eax], edx          ; store instruction in edx where eax points
+    add eax, 4              ; add 4 to eax
+    cmp eax, ebx            ; compare eax and ebx so if eax is less than ebx
+    jl xor1                 ; loop xor1
+                            ; so code of xor1 converts pg code to xor form
+decr:
+    mov eax, pg             ; eax <- reload address of pg
+    mov ebx, pgend          ; ebx <- reload address of pg
+    mov ecx, 0xffffffff     ; ecx <- reassign value of 0xffffffff
+
+xor2:
+    mov edx, [eax]          ; store instruction that pointed by eax in edx
+    xor edx, ecx            ; xor edx, ecx, and store result value in edx
+    mov [eax], edx          ; store instruction in edx where eax points
+    add eax, 4              ; add 4 to eax
+    cmp eax, ebx            ; compare eax and ebx so if eax is less than ebx
+    jl xor2                 ; jmp xor2
+    jmp pg                  ; else jmp pg
+
+halt:
+    jmp halt                ; when program get here, the program ended
+
+pg:
+    add esi, 10             ; meaningless code
+    nop
+    add edi, 10             ; meaningless code
+    nop
+    jmp halt
+    nop
+    nop
+
+pgend:
+
