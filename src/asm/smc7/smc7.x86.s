9995139b6e717ef5e27d9208d2855795ce4c584e
spiderq2697 <65025333+spiderq2697@users.noreply.github.com>
2020-10-07 18:40:34 +0900

    Update smc7.x86.s

--- src/asm/smc7/smc7.x86.s
+++ src/asm/smc7/smc7.x86.s
@@ -5,23 +5,23 @@ global _start
 
 _start:
     nop ; gdb point
-    mov eax, loop ; eax <- addr of loop
+    mov eax, loop   ; eax <- addr of loop
     mov ebx, new
     mov ecx, new
     mov esi, 2
     mov edi, 4
     mov ebp, 0
 loop:
-    mov edx, [eax] ; edx <- instruction of loop
-    mov [ebx], edx ; [ebx] <- store instruction of loop in pointer of ebx
-    add eax, esi ; eax += 2, instruction length is 2byte
-    add ebx, esi ; ebx += 2
-    cmp eax, ecx ; comparison about eax and ecx
-    jne loop ; if not equal, than jump to the loop.
-    mov ecx, ebx ; if equal, all code is copied in new label
+    mov edx, [eax]  ; edx <- instruction of loop
+    mov [ebx], edx  ; [ebx] <- store instruction of loop in pointer of ebx
+    add eax, esi    ; eax += 2, instruction length is 2byte
+    add ebx, esi    ; ebx += 2
+    cmp eax, ecx    ; comparison about eax and ecx
+    jne loop        ; if not equal, than jump to the loop.
+    mov ecx, ebx    ; if equal, all code is copied in new label
     add ebp, esi
     cmp ebp, edi
-    je halt ;halt is relative address, so if we repeat 2 times code, we need to subtract value 
+    je halt         ;halt is relative address, so if we repeat 2 times code, we need to subtract value 
     
 new:
     nop
